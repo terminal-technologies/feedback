871cd7deeb9127f82ab252548c7bb2731cfb876b
terminaltech <>
2022-12-31 03:36:31 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { readFileSync } = require("fs")
+rfs = readFileSync
+const app = express();
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(3000, () => {
+  console.log('server started');
+});
